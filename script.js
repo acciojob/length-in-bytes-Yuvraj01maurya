d1e0dd816825e2719201a28149cbf265a72d6c06
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-12-11 23:44:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const byteSize = (str) => {
   // write your code here
+	const blob = new Blob([str]);
+
+  // Get the size of the Blob in bytes
+  const bytes = blob.size;
+
+  return bytes;
 };
 
 // Do not change the code below
